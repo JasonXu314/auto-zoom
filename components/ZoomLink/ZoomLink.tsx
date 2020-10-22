e6dad7e7eef3dfcebb0e3d52bd457da2f39810f8
Jason Xu <Xinwen31415@gmail.com>
2020-10-22 02:47:25 -0500

    added disarm/rearm toggle

--- components/ZoomLink/ZoomLink.tsx
+++ components/ZoomLink/ZoomLink.tsx
@@ -10,10 +10,17 @@ const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Frida
 
 const ZoomLink: React.FC<Props> = ({ meeting: { url, time, name }, del }) => {
 	const [armed, setArmed] = useState<boolean>(new Date().getDay() === time.weekday);
+	const timeString = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate(), time.hour, time.minute).toLocaleTimeString();
 
 	useEffect(() => {
 		const interval = setInterval(() => {
-			if (new Date().getDay() === time.weekday && !armed) {
+			const day = new Date().getDate();
+			const month = new Date().getMonth();
+			const year = new Date().getFullYear();
+			const target = new Date(year, month, day, time.hour, time.minute).valueOf();
+			const now = Date.now();
+
+			if (new Date().getDay() === time.weekday && !armed && now < target) {
 				setArmed(true);
 			}
 		}, 60000);
@@ -50,12 +57,22 @@ const ZoomLink: React.FC<Props> = ({ meeting: { url, time, name }, del }) => {
 	}, [armed]);
 
 	return (
-		<div className={styles.main}>
-			<div>
-				Meeting {name} at {new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate(), time.hour, time.minute).toLocaleTimeString()}{' '}
+		<div className={styles.main + (armed ? ' ' + styles.active : '')}>
+			<label className={styles.toggle + (armed ? ' ' + styles.checked : '')}>
+				<input
+					type="checkbox"
+					onClick={() => {
+						setArmed(!armed);
+					}}
+					checked={armed}
+				/>
+				<span />
+			</label>
+			<div className={styles.meetingText + (armed ? ' ' + styles.active : '')}>
+				Meeting {name} at {timeString.slice(0, timeString.length - 6) + timeString.slice(timeString.length - 3) + ' '}
 				on {weekdays[time.weekday]}
 			</div>
-			<a href={url} target="_blank" rel="noreferrer noopener" onClick={() => setArmed(false)}>
+			<a className={armed ? styles.active : ''} href={url} target="_blank" rel="noreferrer noopener" onClick={() => setArmed(false)}>
 				Join Manually
 			</a>
 			<button className={styles.delete} onClick={() => del()}>
